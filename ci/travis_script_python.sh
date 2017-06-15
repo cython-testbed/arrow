122d63496ac96356812cf890af88a873accd5031
Robert Bradshaw <robertwb@gmail.com>
2017-06-15 16:16:20 -0700

    Update travis config to point to Cython at 9b3dd8ec9d0b699438bb5f179d4812fbbbae73ed.

--- ci/travis_script_python.sh
+++ ci/travis_script_python.sh
@@ -105,7 +105,7 @@ python_version_tests() {
 
   # Other stuff pip install
   pip install -r requirements.txt
-  pip install --install-option="--no-cython-compile" https://github.com/cython/cython/archive/704ba4dabfacd2621b1db59a4182def6a31674cf.zip
+  pip install --install-option="--no-cython-compile" https://github.com/cython/cython/archive/9b3dd8ec9d0b699438bb5f179d4812fbbbae73ed.zip
 
   python setup.py build_ext --inplace --with-parquet --with-jemalloc
 
